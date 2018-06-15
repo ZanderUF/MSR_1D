b2221b5feec46344a9f592f19fcecaaa9d0d7402
Alexander Mausolff <zanderm@eduroam062-204.wl.anl-external.org>
2018-06-15 17:18:02 -0500

    working on solve.  not getting correct factorization from lapack

--- HeatSolver/solve_soln.f90
+++ HeatSolver/solve_soln.f90
@@ -17,9 +17,16 @@ subroutine solve_soln(nl_iter )
     integer :: nl_iter
 
 !   Local
-    integer :: j, i
+    integer :: j, i, matrix_length, lda, info, lwork
+    integer, dimension(2*num_elem) :: ipiv
+    real (kind=dp), allocatable :: work(:) 
+    allocate(work(2*num_elem))
+    work = 0.0
+    matrix_length = 2*num_elem
+    lda = matrix_length
+    lwork = matrix_length
 
-    allocate(inverse_matrix_K(2*num_elem,2*num_elem) ) 
+    allocate(inverse_matrix_K(matrix_length,matrix_length) ) 
     inverse_matrix_K = 0.0
     
 
@@ -27,7 +34,21 @@ subroutine solve_soln(nl_iter )
     if (steady_state_flag .eqv. .TRUE. ) then
         ! only need to invert K once for steady state
         if( nl_iter .eq. 1) then
-            call inverse(2*num_elem)    
+        !
+            call dgetrf ( matrix_length, matrix_length, final_global_matrix_K, lda, ipiv, info )
+            
+            !if ( info /= 0 ) then
+            !    write ( *, '(a)' ) ' '
+            !    write ( *, '(a,i8)' ) '  DGETRF returned INFO = ', info
+            !    write ( *, '(a)' ) '  The matrix is numerically singular.'
+            !    return
+            !end if
+        print *,'lwork',lwork 
+        !  Compute the inverse matrix.
+        !
+        call dgetri (matrix_length, final_global_matrix_K, lda, ipiv, work, lwork, info ) 
+        
+            print *,'final',final_global_matrix_K
         end if 
         ! otherwise we don't need the inversion
         
@@ -38,7 +59,7 @@ subroutine solve_soln(nl_iter )
         write(outfile_unit,fmt='(a)'),'Inversion of Matrix K- steady state after B.C. applied: '
         do j=1,2*num_elem
                write(outfile_unit,fmt='(12es14.6)') &
-                    (inverse_matrix_K(j,i) ,i=1,2*num_elem)             
+                    (final_global_matrix_K(j,i) ,i=1,2*num_elem)             
         end do
     end if
 
